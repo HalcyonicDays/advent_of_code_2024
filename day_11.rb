0431ba0ab722111b7e580b4aab14d204e68587ac
HalcyonicDays <white.zayetz@gmail.com>
2024-12-11 23:13:41 -0500

    Day 11 still in progress...

--- day_11.rb
+++ day_11.rb
@@ -1,4 +1,4 @@
-initial_contiditions = []
+initial_contiditions = [0, 7, 6618216, 26481, 885, 42, 202642, 8791]
 
 =begin
 Problem: Given some initial starting conditions, apply rules to create new conditions
@@ -35,6 +35,8 @@ Algorithm:
   - split if the number of digits is even,
   - or multiply by 2024 if the number of digits is odd
 - if the value IS a single-digit number
+  - refer to the SPLIT_COUNTS dictionary which keeps tack of each number's
+    progression from itself back to a series of single digits 
 
 
 =end
@@ -67,22 +69,23 @@ def blink(stones, blinks)
   stones
 end
 
-SPLIT_COUNTS = {0 => [1]}
+SPLIT_COUNTS = {}
 def build_split_math
-  1.upto(9) do |single_digit|
+  0.upto(9) do |single_digit|
     SPLIT_COUNTS[single_digit] = []
-    stones = [single_digit * 2024]
+    stones = [single_digit]
+    first_loop = true
 
-    while stones.any? { |stone| stone > 9 }
+    while stones.any? { |stone| stone > 9 } || first_loop
       SPLIT_COUNTS[single_digit] << stones
       stones = stones.map do |stone|
-        if stone > 9
+        if stone > 9 || first_loop
           update_stone(stone)
         else
           stone
         end
       end
-
+      first_loop = false if first_loop
       stones = stones.flatten
     end
     SPLIT_COUNTS[single_digit] << stones
@@ -91,22 +94,33 @@ end
 
 build_split_math
 
-def single_digit_impact(single_digit, remaining_cycles, total_count=1)
-
+def handle_single_digits(digit, remaining_cycles, total_count=0)
+  depth_to_singles = SPLIT_COUNTS[digit].size - 1
+  idx = [depth_to_singles, remaining_cycles].min
+  
+  total_count = SPLIT_COUNTS[digit][idx].size
+  
   while remaining_cycles > 0
-    idx = [SPLIT_COUNTS[single_digit].size - 1, total_count].min
+    idx = [depth_to_singles, remaining_cycles].min
     remaining_cycles -= idx
-    total_count = SPLIT_COUNTS[single_digit][idx].size - 1
 
-    SPLIT_COUNTS[single_digit][idx].each do |single| 
-      total_count += single_digit_impact(single_digit, remaining_cycles, total_count)
+    single_digits = SPLIT_COUNTS[digit][idx].select { |digit| digit <= 9}
+    stragglers = SPLIT_COUNTS[digit][idx].reject { |digit| digit <= 9}
+
+    single_digits.each do |single_digit|
+      puts "this happened"
+      total_count += handle_single_digits(single_digit, remaining_cycles, total_count)-1
     end
+
   end
 
   return total_count
 end
 
-initial_value = 1
-0.upto(2) do |cycles|
-  p [single_digit_impact(initial_value, cycles), blink([1], cycles).size]
+initial_value = 5
+0.upto(4) do |cycles|
+  p handle_single_digits(initial_value, cycles) == blink([1], cycles).size
+  # p handle_single_digits(initial_value, cycles) - blink([1], cycles).size
 end
+
+# p SPLIT_COUNTS
