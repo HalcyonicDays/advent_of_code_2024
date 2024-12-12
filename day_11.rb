1231942315d14f729056b244d40bdcc4dd6b4a0a
HalcyonicDays <white.zayetz@gmail.com>
2024-12-12 11:11:50 -0500

    quick update

--- day_11.rb
+++ day_11.rb
@@ -1,4 +1,4 @@
-initial_contiditions = [0, 7, 6618216, 26481, 885, 42, 202642, 8791]
+initial_contiditions = []
 
 =begin
 Problem: Given some initial starting conditions, apply rules to create new conditions
